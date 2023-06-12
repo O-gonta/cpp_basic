2e8a7044ecf6d056d023d058378df67988492984
ookiy0623 <ookiy0623@outlook.jp>
2023-06-12 23:11:13 +0900

    cout

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	int num = 7;
 	std::cout << "Hello " <<num<<  std::endl;
+	std::cout << "Hello Coding Ocean 2023" << std::endl;
+	std::cout << "„‚Í The Late Commer" << std::endl;
 
 	system("pause>0");
 
