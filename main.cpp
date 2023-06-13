887889df9812b6c3dc2c8673853bd71d473b97ce
ookiy0623 <ookiy0623@outlook.jp>
2023-06-13 15:17:15 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -10,11 +10,16 @@ using namespace std;
         float c = 123.4f;
         char d = 'a'; 
     
-        cout << "a   " << a << "  size   " << sizeof(a) << "  address   " << &a << endl;
-        cout << "b   " << b << "  size   " << sizeof(b) << "  address   " << &b << endl;
-        cout << "c   " << c << "  size   " << sizeof(c) << "  address   " << &c << endl;
-        cout << "d   " << d << "  size   " << sizeof(d) << "  address   " << (void *) &d << endl;
+        cout << "a   " << a << "  size   " << sizeof(a) << "  address   "  << &a << endl;
+        cout << "b   " << b << "  size   " << sizeof(b) << "  address   "  << &b << endl;
+        cout << "c   " << c << "  size   " << sizeof(c) << "  address   "  << &c << endl;
+        cout << "d   " << d << "  size   " << sizeof(d) << "  address   "  << (void *) &d << endl;
+        cout << endl;
 
+        cout << "a   " << a << "  size   " << sizeof(a) << "  address   " << hex << &a << endl;
+        cout << "b   " << b << "  size   " << sizeof(b) << "  address   " << hex << &b << endl;
+        cout << "c   " << c << "  size   " << sizeof(c) << "  address   " << hex << &c << endl;
+        cout << "d   " << d << "  size   " << sizeof(d) << "  address   " << hex << (void*)&d << endl;
 
         /*******************************************
         sizeof演算子の使用方法
